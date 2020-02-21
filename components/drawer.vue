9d6e100adca1a358fc00b18e95ab9884856989e2
platodroid <platodroid@gmail.com>
2020-02-21 16:40:04 +0100

    Initial commit

---
+++ components/drawer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-navigation-drawer app temporary right :value="$store.state.drawerOpen" color="green" @input="close($event)">
+        <v-container>
+        <h1>Drawer</h1>
+        </v-container>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    props: {
+        open: {
+            type: Boolean,
+            open: false
+        }
+    },
+    methods: {
+        close($event) {
+            if (!$event) {
+                this.$store.commit('toggleDrawer')
+            }
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+<v-container>
+<v-btn @click="toggleDrawer()">Open drawer</v-btn>
+<v-btn@click="debuglog(open)">Open value?</v-btn>
+  <Drawer />
+</v-container>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+import Drawer from '@/components/drawer.vue'
+
+export default {
+  components: {
+    Drawer
+  },
+  data: () => ({
+    open: false
+  }),
+
+  methods: {
+    ...mapMutations(['toggleDrawer']),
+    debuglog() {
+      alert(this.open)
+    }
+  }
+}
+</script>
